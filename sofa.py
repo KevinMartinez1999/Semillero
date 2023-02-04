2ca7ca981873b9a7c9456e008d6d42fb199f4f26
KevinMartinez1999 <kevin.martinez1@udea.edu.co>
2023-02-04 14:52:04 -0500

    Cambios en KMeans y DBSCAN.

---
+++ sofa.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+def sync_signals(trama_tx, trama_rx):
+  tx = np.concatenate((trama_tx, trama_tx))
+  corr = np.abs(np.correlate(np.abs(tx) - np.mean(np.abs(tx)),
+                             np.abs(trama_rx) - np.mean(np.abs(trama_rx)), mode='full'))
+  delay = np.argmax(corr) + 1 - len(trama_rx)
+  #print(f'El retraso es de {delay} posiciones')
+  trama_sync = tx[delay:]
+  trama_sync = trama_sync[:len(trama_rx)]
+  return trama_sync
+
+def SymbolErrorRate(sym_rx, sym_tx):
+  error = 0
+  for i, rx in enumerate(sym_rx):
+    if rx != sym_tx[i]:
+      error += 1
+  SER = error / len(sym_tx)
+  return SER, error
+
+def BitErrorRate(sym_rx, sym_tx):
+  sym_rx_bin = ''.join([f'{sym:04b}' for sym in sym_rx])
+  sym_tx_bin = ''.join([f'{sym:04b}' for sym in sym_tx])
+
+  error = 0
+  for i in range(len(sym_tx_bin)):
+    if sym_rx_bin[i] != sym_tx_bin[i]:
+      error += 1
+  BER = error / len(sym_tx_bin)
+  return BER, error
+
+def demapper_sym(symbols_I, symbols_Q, Ns, threshold = 2.0):
+   symbol = []
+   for i in range(Ns):
+        if symbols_I[i] <= -threshold and symbols_Q[i] >= threshold: #-3+3j
+            symbol.append(0)
+        elif symbols_I[i] <= -threshold and symbols_Q[i] >= 0 and symbols_Q[i] <= threshold: #-3+1j
+            symbol.append(1)
+        elif symbols_I[i] <= -threshold and symbols_Q[i] <= 0 and symbols_Q[i] >= -threshold: #-3-j
+            symbol.append(3)
+        elif symbols_I[i] <= -threshold and symbols_Q[i] <= -threshold: #-3-3j
+            symbol.append(2)
+        elif symbols_I[i] >= -threshold and symbols_I[i] <= 0 and symbols_Q[i] >= threshold: #-1+3j
+            symbol.append(4)
+        elif symbols_I[i] >= -threshold and symbols_I[i] <= 0 and symbols_Q[i] >= 0 and symbols_Q[i] <= threshold: #-1+j
+            symbol.append(5)
+        elif symbols_I[i] >= -threshold and symbols_I[i] <= 0 and symbols_Q[i] <= 0 and symbols_Q[i] >= -threshold: #-1-j
+            symbol.append(7)
+        elif symbols_I[i] >= -threshold and symbols_I[i] <= 0 and symbols_Q[i] <= -threshold: #-1-3j
+            symbol.append(6)
+        elif symbols_I[i] >= 0 and symbols_I[i] <= threshold and symbols_Q[i] >= threshold: #1+3j
+            symbol.append(12)
+        elif symbols_I[i] >= 0 and symbols_I[i] <= threshold and symbols_Q[i] >= 0 and symbols_Q[i] <= threshold: #1+j
+            symbol.append(13)
+        elif symbols_I[i] >= 0 and symbols_I[i] <= threshold and symbols_Q[i] <= 0 and symbols_Q[i] >= -threshold: #1-j
+            symbol.append(15)
+        elif symbols_I[i] >= 0 and symbols_I[i] <= threshold and symbols_Q[i] <= -threshold: #1-3j
+            symbol.append(14)
+        elif symbols_I[i] >= threshold and symbols_Q[i] >= threshold: #3+3j
+            symbol.append(8)
+        elif symbols_I[i] >= threshold and symbols_Q[i] >= 0 and symbols_Q[i] <= threshold: #3+1j
+            symbol.append(9)
+        elif symbols_I[i] >= threshold and symbols_Q[i] <= 0 and symbols_Q[i] >= -threshold: #3-1j
+            symbol.append(11)
+        elif symbols_I[i] >= threshold and symbols_Q[i] <= -threshold: #3-3j
+            symbol.append(10)
+   return(symbol)
+
+def demodulate(arr):  
+  y = []
+  for i in arr:
+    if i == -3+3j:
+      y.append(0)
+    elif i == -3+1j:
+      y.append(1)
+    elif i == -3-3j:
+      y.append(2)
+    elif i == -3-1j:
+      y.append(3)
+    elif i == -1+3j:
+      y.append(4)
+    elif i == -1+1j:
+      y.append(5)
+    elif i == -1-3j:
+      y.append(6)
+    elif i == -1-1j:
+      y.append(7)
+    elif i == 3+3j:
+      y.append(8)
+    elif i == 3+1j:
+      y.append(9)
+    elif i == 3-3j:
+      y.append(10)
+    elif i == 3-1j:
+      y.append(11)
+    elif i == 1+3j:
+      y.append(12)
+    elif i == 1+1j:
+      y.append(13)
+    elif i == 1-3j:
+      y.append(14)
+    elif i == 1-1j:
+      y.append(15)
+  return y
+
